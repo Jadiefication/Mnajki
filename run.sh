2e93a663923a3720d258845f8b9f0310e6b36280
jadie <jadeczjade008@gmail.com>
2026-04-23 08:57:11 +0200

    chore: fix run script

--- run.sh
+++ run.sh
@@ -8,3 +8,11 @@ clang -target aarch64-none-elf -c start.asm -o start.o
 # -e _start sets the entry point
 # --section-start sets the load address for QEMU's 'virt' machine
 ld.lld start.o -o start.elf -e _start --section-start .text=0x40000000
+
+# 3. Run in QEMU
+qemu-system-aarch64 \
+    -M virt \
+    -cpu cortex-a57 \
+    -display none \
+    -serial stdio \
+    -kernel start.elf
